907f9f953b9fb44d703197eeef923b7c04bce91c
hackerman <jingyao990@gmail.com>
2024-05-25 00:34:36 +0200

    fixed bug in saving child tasks to dict

--- parserFile.py
+++ parserFile.py
@@ -1,5 +1,6 @@
 from xml.dom.minidom import parse, Node, parseString
 
+
 # Set the id attribute for all elements that have an attribute named "id"
 def set_id_attribute(parent, attribute_name="id"):
     if parent.nodeType == Node.ELEMENT_NODE:
@@ -8,16 +9,16 @@ def set_id_attribute(parent, attribute_name="id"):
     for child in parent.childNodes:
         set_id_attribute(child, attribute_name)
 
-# Parse XML from a filename
-document = parse("/Users/jin/jin-git/HiWi/diagram (3).bpmn")
-set_id_attribute(document)
 
+# Parse XML from a filename
+document = parse("/Users/jin/jin-git/HiWi/data_Samples/diagram (3).bpmn")
+set_id_attribute(document)
 
 # Activities
 # Save all task IDs in a list
 taskIds = []
 for element in document.getElementsByTagNameNS("*", "task"):
-  taskIds.append(element.getAttribute("id"))
+    taskIds.append(element.getAttribute("id"))
 print(f"\nTask IDs: {taskIds} \n")
 
 # Count the number of tasks
@@ -25,20 +26,23 @@ taskCount = len(taskIds)
 print(f"How many tasks do we have? {taskCount}\n")
 
 taskList = []
-dfa = {0:{'0':0, '1':1},}
+dfa = {0: {'0': 0, '1': 1}, }
 
 for idx in range(0, taskCount):
     elements = document.getElementsByTagNameNS("*", "task")
     if not elements:
         continue
     taskList.append(elements[0])
-    
-for child in taskList[0].childNodes:
-    if child.nodeType == Node.ELEMENT_NODE:
-        if child.tagName == "bpmn:incoming":
-            dfa.__setitem__("incoming",{child.firstChild.data})
-        elif child.tagName == "bpmn:outgoing":
-            dfa.__setitem__("outgoing", {child.firstChild.data})
+
+for idx in range(0, taskCount):
+    for child in taskList[0].childNodes:
+        if child.nodeType == Node.ELEMENT_NODE:
+            if child.tagName == "bpmn:incoming":
+                dfa.__setitem__(1, child.firstChild.data)
+            elif child.tagName == "bpmn:outgoing":
+                dfa.__setitem__(2, child.firstChild.data)
+print(len(taskList))
+print(f"Here's the DFA {dfa}")
 
 # Gateways
 # Get all gateway IDs
@@ -49,12 +53,11 @@ for gatewayType in gatewayTypes:
         gatewayIds.append(element.getAttribute("id"))
 print(f"\nGateway IDs: {gatewayIds} \n")
 gatewayCount = len(gatewayIds)
-print(f"How many gateways do we have? {gatewayCount/2}\n")
-
+print(f"How many gateways do we have? {gatewayCount / 2}\n")
 
 # Retrieve first gateway and child nodes
 for gatewayType in gatewayTypes:
-    elements = document.getElementsByTagNameNS("*",gatewayType)
+    elements = document.getElementsByTagNameNS("*", gatewayType)
     if not elements:
         continue
     gateway = elements[0]
@@ -67,7 +70,6 @@ for child in gateway.childNodes:
             incoming.append(child.firstChild.data)
         elif child.tagName == "bpmn:outgoing":
             outgoing.append(child.firstChild.data)
-            
 
 print(gateway.getAttribute("id"))
 print(incoming)
