9751faefae4d7a2e3a4351887b02b468b89709b5
hackerman <jingyao990@gmail.com>
2024-06-06 20:41:41 +0200

    Extended functionality to include gateways in DFA tree.
    -GatewayList stores and adds a numerated xml attribute if it doesn't exist to each element.
    -Wrote loop to iterate and add gateway elements to the DFA tree

--- xmlParser.py
+++ xmlParser.py
@@ -1,4 +1,5 @@
 from xml.dom.minidom import parse, Node, parseString
+import Gateways, Activities
 
 
 # Set the id attribute for all elements that have an attribute named "id"
@@ -9,15 +10,15 @@ def set_id_attribute(parent, attribute_name="id"):
     for child in parent.childNodes:
         set_id_attribute(child, attribute_name)
 
-
 # Parse XML from a filename
-document = parse("/Users/jin/jin-git/HiWi/data/diagram (3).bpmn")
+document = parse("/Users/jin/jin-git/HiWi/data/deliveryProcess.bpmn")
 set_id_attribute(document)
 
 # Activities
 # Save all task IDs in a list
 taskIds = []
 for element in document.getElementsByTagNameNS("*", "task"):
+    # activity = Activities(element.getAttribute("id"), element.getAttribute("name"), "", "")
     taskIds.append(element.getAttribute("id"))
 print(f"\nTask IDs: {taskIds} \n")
 
@@ -41,47 +42,66 @@ for idx in range(0, taskCount):
         dfa[task_name] = {}
     for child in taskList[idx].childNodes:
         if child.nodeType == Node.ELEMENT_NODE:
-            if child.tagName == "bpmn:incoming":
+            if child.tagName == "incoming":
                 dfa[task_name]['incoming'] = child.firstChild.data
-            elif child.tagName == "bpmn:outgoing":
+            elif child.tagName == "outgoing":
                 dfa[task_name]['outgoing'] = child.firstChild.data
 
 print(f"Task list length: {len(taskList)}")
 print(taskList[3].getAttribute("id"))
 print(f"Here's the incoming of the first task {dfa.get(str(taskList[0].getAttribute('name'))).get('incoming')}")
 print(f"Here's the outgoing of the first task {dfa.get(str(taskList[0].getAttribute('name'))).get('outgoing')}")
-print(f"Here's the DFA {dfa}")
+print(f"Here's the task DFA {dfa}")
 
-#TODO Match the incoming and outgoing of the tasks
-# for idx in range(0, taskCount):
-   
 
+#TODO inclusive gateway must remember which activities have executed
 
 # Gateways
 # Get all gateway IDs
 gatewayTypes = ["exclusiveGateway", "inclusiveGateway", "parallelGateway", "complexGateway"]
 gatewayIds = []
-for gatewayType in gatewayTypes:
-    for element in document.getElementsByTagNameNS("*", gatewayType):
-        gatewayIds.append(element.getAttribute("id"))
-print(f"\nGateway IDs: {gatewayIds} \n")
-gatewayCount = len(gatewayIds)
-print(f"How many gateways do we have? {gatewayCount / 2}\n")
+gatewayIdx = 0
+gatewayList = []
 
-# Retrieve first gateway and child nodes
 for gatewayType in gatewayTypes:
-    elements = document.getElementsByTagNameNS("*", gatewayType)
+    gatewayIdx += 1
+    for element in document.getElementsByTagNameNS("*", gatewayType):
+        gatewayIds.append(str(gatewayType) + f"{gatewayIdx}: " + element.getAttribute("id"))
+        gatewayList.append(element)
+        element.setAttribute("name", str(gatewayType) + f"{gatewayIdx}: " + element.getAttribute("id"))
+
+print(f"\nGateway List: {gatewayIds} \n")
+print(f"How many gateways do we have? {len(gatewayIds) / 2}\n")
+
+gatewayCount = len(gatewayIds)
+
+
+for idx in range(0, gatewayCount):
+    elements = document.getElementsByTagNameNS("*", "gateway")
     if not elements:
         continue
-    gateway = elements[0]
+    gatewayList.append(elements[idx]) # Saves all task elements in a list
 
-incoming = []
-outgoing = []
-for child in gateway.childNodes:
-    if child.nodeType == Node.ELEMENT_NODE:
-        if child.tagName == "bpmn:incoming":
-            incoming.append(child.firstChild.data)
-        elif child.tagName == "bpmn:outgoing":
-            outgoing.append(child.firstChild.data)
+for idx in range(0, gatewayCount):
+    gatewayName = str(gatewayList[idx].getAttribute("name"))
+    if gatewayName not in dfa:
+        dfa[gatewayName] = {}
+    for child in gatewayList[idx].childNodes:
+        if child.nodeType == Node.ELEMENT_NODE:
+            if child.tagName == "incoming":
+                dfa[gatewayName]['incoming'] = child.firstChild.data
+            elif child.tagName == "outgoing":
+                dfa[gatewayName]['outgoing'] = child.firstChild.data
 
-print(gateway.getAttribute("id"))
+print(f"Here's the gateway DFA{dfa}")
+
+
+#TODO Match the incoming and outgoing of tasks
+for idx in range(0, taskCount):
+    for index in range(0, taskCount):
+        if dfa.get(str(taskList[idx].getAttribute('name'))).get('outgoing') == dfa.get(str(taskList[index].getAttribute('name'))).get('incoming'):
+            print(f"Matched {taskList[idx].getAttribute('name')} and {taskList[index].getAttribute('name')}")
+        else:
+            print(f"Didn't match {taskList[idx].getAttribute('name')} and {taskList[index].getAttribute('name')}")
+
+
